aeff6a8cb0cb29a9b34f04aa4d8bce794c13c0e9
Prasheek-13 <prasheekpatle@gmail.com>
2026-06-30 15:50:14 +0530

    Time: 0 ms (100%), Space: 10.9 MB (34.04%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -14,15 +14,29 @@ class Solution {
 public:
     vector<int> ans = {};
     vector<int> inorderTraversal(TreeNode* root) {
-
         if (root == NULL) {
             return ans;
         }
-
-        inorderTraversal(root->left);
-        ans.push_back(root->val);
-        inorderTraversal(root->right);
-
+        TreeNode* curr = root;
+        while (curr != NULL) {
+            if (curr->left == NULL) {
+                ans.push_back(curr->val);
+                curr = curr->right;
+            } else {
+                TreeNode* ip = curr->left;
+                while (ip->right != NULL && ip->right != curr) {
+                    ip = ip->right;
+                }
+                if (ip->right == NULL) {
+                    ip->right = curr;
+                    curr = curr->left;
+                } else {
+                    ip->right = NULL;
+                    ans.push_back(curr->val);
+                    curr = curr->right;
+                }
+            }
+        }
         return ans;
     }
 };
