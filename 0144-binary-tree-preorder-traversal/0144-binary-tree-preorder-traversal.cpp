5b28156a55b82af773c0540797e1df943b0a2058
Prasheek-13 <prasheekpatle@gmail.com>
2026-06-30 15:17:29 +0530

    Time: 0 ms (100%), Space: 10.9 MB (73.73%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -10,17 +10,19 @@
  * right(right) {}
  * };
  */
+void preorder(TreeNode* root, vector<int>& ans) {
+    if (root == NULL)
+        return;
+    ans.push_back(root->val);
+    preorder(root->left, ans);
+    preorder(root->right, ans);
+}
+
 class Solution {
 public:
-    vector<int> ans = {};
     vector<int> preorderTraversal(TreeNode* root) {
-        if (root == NULL) {
-            return ans;
-        }
-        ans.push_back(root->val);
-        preorderTraversal(root->left);
-        preorderTraversal(root->right);
-
+        vector<int> ans;
+        preorder(root, ans);
         return ans;
     }
 };
