68e3ae78f9c61d7fdba8947b5727ab87473ac178
Prasheek-13 <prasheekpatle@gmail.com>
2026-07-09 16:21:01 +0530

    Time: 9 ms (26.97%), Space: 19.2 MB (68.28%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,31 +1,44 @@
 class Solution {
 public:
     bool dfs(int curr, vector<bool>& vis, vector<bool>& recpath,
-             vector<vector<int>>& edges) {
+             vector<vector<int>>& adj) {
         vis[curr] = true;
         recpath[curr] = true;
-        for (int i = 0; i < edges.size(); i++) {
-            int u = edges[i][1];
-            int v = edges[i][0];
-            if (curr == u) {
-                if (!vis[v]) {
-                    if (dfs(v, vis, recpath, edges)) {
-                        return true;
-                    }
-                } else if (recpath[v]) {
+        // for (int i = 0; i < edges.size(); i++) {
+        //     int u = edges[i][1];
+        //     int v = edges[i][0];
+        //     if (curr == u) {
+        //         if (!vis[v]) {
+        //             if (dfs(v, vis, recpath, edges)) {
+        //                 return true;
+        //             }
+        //         } else if (recpath[v]) {
+        //             return true;
+        //         }
+        //     }
+        // }
+        for (int v : adj[curr]) {
+            if (!vis[v]) {
+                if (dfs(v, vis, recpath, adj)) {
                     return true;
                 }
+            } else if (recpath[v]) {
+                return true;
             }
         }
         recpath[curr] = false;
         return false;
     }
     bool canFinish(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n);
+        for (auto& p : edges) {
+            adj[p[1]].push_back(p[0]);
+        };
         vector<bool> vis(n, false);
         vector<bool> recpath(n, false);
         for (int i = 0; i < n; i++) {
             if (!vis[i]) {
-                if (dfs(i, vis, recpath, edges)) {
+                if (dfs(i, vis, recpath, adj)) {
                     return false;
                 }
             }
