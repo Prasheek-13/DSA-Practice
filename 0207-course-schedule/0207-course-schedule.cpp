96ec66346626eef50f0e11a30c953fe0d8f764d9
Prasheek-13 <prasheekpatle@gmail.com>
2026-07-10 15:53:19 +0530

    Time: 2 ms (79.37%), Space: 19.3 MB (68.26%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,48 +1,44 @@
 class Solution {
 public:
-    bool dfs(int curr, vector<bool>& vis, vector<bool>& recpath,
-             vector<vector<int>>& adj) {
-        vis[curr] = true;
-        recpath[curr] = true;
-        // for (int i = 0; i < edges.size(); i++) {
-        //     int u = edges[i][1];
-        //     int v = edges[i][0];
-        //     if (curr == u) {
-        //         if (!vis[v]) {
-        //             if (dfs(v, vis, recpath, edges)) {
-        //                 return true;
-        //             }
-        //         } else if (recpath[v]) {
-        //             return true;
-        //         }
-        //     }
-        // }
-        for (int v : adj[curr]) {
-            if (!vis[v]) {
-                if (dfs(v, vis, recpath, adj)) {
-                    return true;
-                }
-            } else if (recpath[v]) {
-                return true;
+    bool toposort(vector<vector<int>>& adj) {
+        // calculate indgree
+        vector<int> res;
+        vector<int> indeg(adj.size(), 0);
+        for (int u = 0; u < adj.size(); u++) {
+            for (int v : adj[u]) {
+                indeg[v]++;
             }
         }
-        recpath[curr] = false;
-        return false;
+        // 0 indeg ==> Q
+        queue<int> q;
+        for (int i = 0; i < indeg.size(); i++) {
+            if (indeg[i] == 0) {
+                q.push(i);
+            }
+        }
+        // bfs
+        while (q.size() > 0) {
+            int curr = q.front();
+            q.pop();
+            res.push_back(curr);
+            for (int v : adj[curr]) {
+                indeg[v]--;
+                if (indeg[v] == 0) {
+                    q.push(v);
+                }
+            }
+        }
+        // result
+        if (res.size() != adj.size()) {
+            return false;
+        }
+        return true;
     }
     bool canFinish(int n, vector<vector<int>>& edges) {
         vector<vector<int>> adj(n);
         for (auto& p : edges) {
             adj[p[1]].push_back(p[0]);
         };
-        vector<bool> vis(n, false);
-        vector<bool> recpath(n, false);
-        for (int i = 0; i < n; i++) {
-            if (!vis[i]) {
-                if (dfs(i, vis, recpath, adj)) {
-                    return false;
-                }
-            }
-        }
-        return true;
+        return toposort(adj);
     }
 };
