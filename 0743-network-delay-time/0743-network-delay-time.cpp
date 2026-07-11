abf91108dd01fc06fdab5a7e2e78b595bd84d5e0
Prasheek-13 <prasheekpatle@gmail.com>
2026-07-11 18:39:42 +0530

    Time: 80 ms (69.77%), Space: 44.3 MB (82.58%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<vector<pair<int, int>>> adj(n + 1);
-
-        for (auto& t : times) {
-            int u = t[0];
-            int v = t[1];
-            int wt = t[2];
+        for (auto& e : times) {
+            int u = e[0];
+            int v = e[1];
+            int wt = e[2];
             adj[u].push_back({v, wt});
         }
         vector<int> dist(n + 1, INT_MAX);
@@ -14,8 +13,8 @@ public:
         priority_queue<pair<int, int>, vector<pair<int, int>>,
                        greater<pair<int, int>>>
             pq;
-        pq.push({0, k});
-        while (pq.size() > 0) {
+        pq.push({dist[k], k});
+        while (!pq.empty()) {
             int u = pq.top().second;
             pq.pop();
             for (auto e : adj[u]) {
@@ -29,8 +28,9 @@ public:
         }
         int ans = 0;
         for (int i = 1; i <= n; i++) {
-            if (dist[i] == INT_MAX)
+            if (dist[i] == INT_MAX) {
                 return -1;
+            }
             ans = max(ans, dist[i]);
         }
         return ans;
