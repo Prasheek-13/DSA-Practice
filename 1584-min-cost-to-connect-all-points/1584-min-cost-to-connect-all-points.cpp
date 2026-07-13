4bee1e3b7db00474bdc0faa84501b407557d8f45
Prasheek-13 <prasheekpatle@gmail.com>
2026-07-13 16:13:24 +0530

    Time: 869 ms (13.44%), Space: 166 MB (22.42%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -3,13 +3,10 @@ public:
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n = points.size();
         vector<vector<pair<int, int>>> adj(n);
-
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-
                 int wt = abs(points[i][0] - points[j][0]) +
                          abs(points[i][1] - points[j][1]);
-
                 adj[i].push_back({j, wt});
                 adj[j].push_back({i, wt});
             }
@@ -19,18 +16,16 @@ public:
         priority_queue<pair<int, int>, vector<pair<int, int>>,
                        greater<pair<int, int>>>
             pq;
-        int mstcost = 0;
         pq.push({0, 0});
+        int mstcost = 0;
         while (!pq.empty()) {
             auto p = pq.top();
             int wt = p.first;
             int u = p.second;
             pq.pop();
-
             if (!InMST[u]) {
                 InMST[u] = true;
                 mstcost = wt + mstcost;
-
                 for (int i = 0; i < adj[u].size(); i++) {
                     int v = adj[u][i].first;
                     int w = adj[u][i].second;
