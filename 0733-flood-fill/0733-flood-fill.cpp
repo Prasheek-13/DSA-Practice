7181c65a3366affea417f999ac20b38e2372077a
Prasheek-13 <prasheekpatle@gmail.com>
2026-07-19 16:46:27 +0530

    Time: 0 ms (100%), Space: 18 MB (90.9%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     void dfs(vector<vector<int>>& image, int sr, int sc, int newcolor,
-             int orignalcolor) {
+             int originalcolor) {
         if (sr < 0 || sc < 0 || sr >= image.size() || sc >= image[0].size() ||
-            image[sr][sc] != orignalcolor || image[sr][sc] == newcolor) {
+            image[sr][sc] != originalcolor || image[sr][sc] == newcolor) {
             return;
         }
         image[sr][sc] = newcolor;
-        dfs(image, sr - 1, sc, newcolor, orignalcolor);
-        dfs(image, sr + 1, sc, newcolor, orignalcolor);
-        dfs(image, sr, sc - 1, newcolor, orignalcolor);
-        dfs(image, sr, sc + 1, newcolor, orignalcolor);
+        dfs(image, sr - 1, sc, newcolor, originalcolor);
+        dfs(image, sr + 1, sc, newcolor, originalcolor);
+        dfs(image, sr, sc - 1, newcolor, originalcolor);
+        dfs(image, sr, sc + 1, newcolor, originalcolor);
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
                                   int color) {
-        int orignalcolor = image[sr][sc];
-        dfs(image, sr, sc, color, orignalcolor);
+        int oricolor = image[sr][sc];
+        dfs(image, sr, sc, color, oricolor);
         return image;
     }
 };
