ae61bd5d82132706294c3fac3f7918897f2971f5
Prasheek-13 <prasheekpatle@gmail.com>
2026-07-30 10:47:32 +0530

    Time: 31 ms (42.61%), Space: 18.4 MB (85.61%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -31,7 +31,7 @@ public:
             par[i] = i;
         }
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = i + 1; j < n; j++) {
                 if (stones[i][0] == stones[j][0] ||
                     stones[i][1] == stones[j][1]) {
                     unionD(i, j);
