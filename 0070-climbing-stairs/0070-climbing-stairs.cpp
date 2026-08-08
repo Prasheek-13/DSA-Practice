f7ed2d760df81b4c78e7bb97dd5a0cd2e1e48263
Prasheek-13 <prasheekpatle@gmail.com>
2026-08-09 00:40:00 +0530

    Time: 0 ms (100%), Space: 7.9 MB (59.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -4,12 +4,15 @@ public:
         if (n == 1 || n == 2) {
             return n;
         }
-        vector<int> dp(n + 1);
-        dp[1] = 1;
-        dp[2] = 2;
+        int prev1 = 2;
+        int prev2 = 1;
+        int result = prev1;
+
         for (int i = 3; i < n + 1; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            result = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = result;
         }
-        return dp[n];
+        return result;
     }
 };
