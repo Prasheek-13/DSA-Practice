6bf486c601db3cdee67e4e4e1d03fcb1c5e2c9f7
Prasheek-13 <prasheekpatle@gmail.com>
2026-08-15 23:56:26 +0530

    Time: 130 ms (56.68%), Space: 111.4 MB (13.69%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -8,23 +8,22 @@ public:
         if (dp[i][target] != -1) {
             return dp[i][target];
         }
-        bool notTake = solve(nums, i - 1, target, dp);
+        bool nottake = solve(nums, i - 1, target, dp);
         bool take = false;
         if (nums[i - 1] <= target) {
             take = solve(nums, i - 1, target - nums[i - 1], dp);
         }
-
-        return dp[i][target] = take || notTake;
+        return dp[i][target] = take || nottake;
     }
     bool canPartition(vector<int>& nums) {
         int sum = 0;
         for (int i = 0; i < nums.size(); i++) {
             sum += nums[i];
         }
-        int target = sum / 2;
         if (sum % 2 != 0) {
             return false;
         }
+        int target = sum / 2;
         vector<vector<int>> dp(nums.size() + 1, vector<int>(target + 1, -1));
         return solve(nums, nums.size(), target, dp);
     }
