41df47fe5a005f6b3e30d06c8f62816849fcef59
Prasheek-13 <prasheekpatle@gmail.com>
2026-09-09 22:42:21 +0530

    Time: 0 ms (100%), Space: 9.2 MB (7.73%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     long long countCommas(long long n) {
-        long long sum = 0;
-        if (n < 1000) {
-            sum = 0;
-        }
-        if (1000 <= n && n < 1000000) {
-            sum = n - 999;
-        } else if (1000000 <= n && n < 1000000000) {
-            sum = 2 * n - 999 - 999999;
-        } else if (1000000000 <= n && n < 1000000000000) {
-            sum = 3 * n - 999 - 999999 - 999999999;
-        } else if (1000000000000 <= n && n < 1000000000000000) {
-            sum = 4 * n - 999 - 999999 - 999999999 - 999999999999;
-        } else if (n == 1000000000000000) {
-            sum = 3998998998999005;
+        long long comma = 0;
+        long long base = 1000;
+        if (n < 1000)
+            return 0;
+        else {
+            while (base <= n) {
+                comma += (n - base + 1);
+                base *= 1000;
+            }
         }
 
-        return sum;
+        return comma;
     }
 };
